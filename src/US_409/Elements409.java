cbb1f288b95140bfca3b34b7d55e9bccb2fe6ea7
ermankaratash <48660429+ermankaratash@users.noreply.github.com>
2024-03-23 12:12:35 +0100

    locators created and new BaseDriverErm file created

--- src/US_409/Elements409.java
+++ src/US_409/Elements409.java
@@ -1,4 +1,29 @@
 package US_409;
 
-public class Elements409 {
+import Utility.BaseDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class Elements409  {
+
+    public void Elements409(){
+        PageFactory.initElements(BaseDriver.driver, this);
+    }
+    @FindBy(id = "coreapps-datamanagement-homepageLink-coreapps-datamanagement-homepageLink-extension")
+    public WebElement dataManagementButton;
+    @FindBy(id = "coreapps-mergePatientsHomepageLink-app")
+    public WebElement mergeButton;
+    @FindBy(id = "patient1-text")
+    public WebElement patient1;
+    @FindBy(id = "patient2-text")
+    public WebElement patient2;
+    @FindBy(id = "confirm-button")
+    public WebElement confirmButton;
+    @FindBy(id = "first-patient")
+    public WebElement firstPatient;
+    @FindBy(xpath = "//div[@class='float-sm-right']/span")
+    public List<WebElement> patients;
 }
--- src/US_409/Patient_Record_Merge.java
+++ src/US_409/Patient_Record_Merge.java
@@ -1,11 +1,10 @@
 package US_409;
 
 import Utility.BaseDriver;
+import Utility.BaseDriverErm;
 import org.testng.annotations.Test;
 
-public class Patient_Record_Merge extends BaseDriver { // Erman
-
-
+public class Patient_Record_Merge extends BaseDriverErm { // Erman
      @Test
      public void TC_0901(){
 
