b0aa10ffad1bb49ca1e6935cae200978e7b7d2cc
Admin <sidagulov@gmail.com>
2024-03-25 01:02:08 +0200

    Completed US405

--- src/US_405/Elements_405.java
+++ src/US_405/Elements_405.java
@@ -13,8 +13,20 @@ public class Elements_405 {
         PageFactory.initElements(BaseDriver.driver, this);
     }
 
+    @FindBy(xpath = "//*[@class='icon-user small']")
+    public WebElement adminIcon;
 
+    @FindBy(xpath = "//*[@id='user-account-menu']//a")
+    public WebElement myAccountButton;
 
+    @FindBy(xpath = "//div[@id='tasks']//div")
+    public List<WebElement> tasks;
+
+    @FindBy(xpath = "//div[@id='tasks']//a")
+    public List<WebElement> buttonApp;
+
+    @FindBy(xpath = "//*[@id='cancel-button']")
+    public WebElement cancelButton;
 
 
 
--- src/US_405/My_Account.java
+++ src/US_405/My_Account.java
@@ -4,6 +4,8 @@ import Utility.BaseDriver;
 import Utility.GWD;
 import Utility.LCE;
 import Utility.Tools;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
@@ -14,9 +16,31 @@ public class My_Account extends BaseDriver { // Serik
     @Test
     public void TC_0501() {
 
+        Actions driverAction = new Actions(driver);
+        Elements_405 tc05 = new Elements_405();
 
+        driverAction.moveToElement(tc05.adminIcon).build().perform();
 
+        tc05.myAccountButton.click();
+
+        // Password
+        //Languages
+        for (WebElement e : tc05.tasks) {
+            if (e.getText().contains("Password") || e.getText().contains("Languages")) {
+                System.out.println(e.getText().trim() + " visible");
+            }
+        }
+
+        for (WebElement e: tc05.buttonApp){
+            e.click();
+            wait.until(ExpectedConditions.elementToBeClickable(tc05.cancelButton));
+
+            tc05.cancelButton.click();
+            wait.until(ExpectedConditions.visibilityOfAllElements(tc05.buttonApp));
+
+        }
 
     }
+
 }
 
