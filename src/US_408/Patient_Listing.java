bc4b56c0d26afca3a9063ee6471de67d35c8d0ac
burak <burak_demi1@web.de>
2024-03-24 20:55:28 +0100

    US408 Test passed

--- src/US_408/Patient_Listing.java
+++ src/US_408/Patient_Listing.java
@@ -1,6 +1,9 @@
 package US_408;
 
+import US_407.Elements_407;
 import Utility.BaseDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Patient_Listing extends BaseDriver { // Burak
@@ -9,5 +12,20 @@ public class Patient_Listing extends BaseDriver { // Burak
     @Test
     public void TC_0801(){
 
+        Elements_408 tc08=new Elements_408();
+
+        tc08.findPatient.click();
+        wait.until(ExpectedConditions.visibilityOfAllElements(tc08.patientList));
+
+        String dataTablesReplace=tc08.showingPatientEntries.getText().replaceAll("[^0-9]","");
+
+        String dataTableSubs=dataTablesReplace.substring(2);
+
+        int dataTableZ=Integer.parseInt(dataTableSubs);
+        //System.out.println("dataTableZ = " + dataTableZ);
+        int listSize=tc08.patientList.size();
+        //System.out.println("listSize = " + listSize);
+
+        Assert.assertEquals(dataTableZ,listSize,"The patient Number is not equal");
     }
 }
