0d58036dd863a1e4a118b8b5b88ded687ffa4775
turuhsen <tubaaldemir1013@gmail.com>
2024-03-25 22:57:42 +0100

    Checking Login added

--- src/US_402/Elements_402.java
+++ src/US_402/Elements_402.java
@@ -1,6 +1,7 @@
 package US_402;
 
 import Utility.BaseDriver;
+import Utility.GWD;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
