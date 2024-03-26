1790f1aaabbb2b19058b5a54245c6c6d4f00d6fe
PC <sudabagurbanli@yahoo.com>
2024-03-26 21:09:46 +0100

    US403 completed.

--- src/US_403/Perform_Logout_from_the_System.java
+++ src/US_403/Perform_Logout_from_the_System.java
@@ -23,7 +23,7 @@ public class Perform_Logout_from_the_System extends BaseDriver { // Sudaba
         wait.until(ExpectedConditions.urlContains("login"));
 
         String url=driver.getCurrentUrl();
-        Assert.assertTrue(url.contains("login"),"Loginpage could not be reached ");
+        Assert.assertTrue(url.contains("login"),"Loginpage could not be reached. ");
 
 
 
